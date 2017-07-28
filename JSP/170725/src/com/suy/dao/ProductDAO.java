3feb25e202f90bbf30a4b92a500ff01bae26de6d
UiyoungSeo <suy2suy@gmail.com>
2017-07-28 14:21:58 +0900

    file upload with cos library

--- JSP/170725/src/com/suy/dao/ProductDAO.java
+++ JSP/170725/src/com/suy/dao/ProductDAO.java
@@ -121,4 +121,21 @@ public class ProductDAO {
 			DBManager.close(conn, pstmt);
 		}
 	}
+
+	// c r u Delete
+	public void deleteProduct(String code) {
+		String sql = "delete product where code=?";
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		try {
+			conn = DBManager.getConnection();
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, code);
+			pstmt.executeUpdate();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			DBManager.close(conn, pstmt);
+		}
+	}
 }
